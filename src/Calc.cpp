fe2351a7cb23ad4ef7f0224d766b6bb163996d94
Alexander Helbok <alexander.helbok@hotmail.com>
2022-05-02 20:46:37 +0200

    sleep implemented

--- src/Calc.cpp
+++ src/Calc.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include "Calc.h"
 
-int TimeCorrection(double offset, double time, double speed){
-    double i, sum = 0;
+int TimeCorrection(double offset, double time, double speed){               // offset = steps/1sec, time in secs, speed in clock secs/real sec
+    int i;
+    double sum = 0;
     if (time <= 21276){
         for (i = 0; i < 11; ++i){
-            sum = sum + offset * time/pow(speed,i);
-            printf("%.5f\n",sum);
+            sum += offset * time/pow(speed,i);
+            // printf("%.5f\n",sum);
         } 
     }
     else{
         time = 43200 - time;
         for (i = 0; i < 11; ++i){
-            sum = sum + offset * pow(-1,i) * time/pow(speed,i);
-            printf("%.5f\n",sum);
+            sum += offset * pow(-1,i) * time/pow(speed,i);
+            // printf("%.5f\n",sum);
         }   
     }
     return int(lround(sum));
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
-#include "A4988.h"
+#include "BasicStepperDriver.h"
 #include <Wire.h>
 #include "Wifi.h"
 #include "NTP.h"
 #include "Calc.h"
+#include "user_interface.h"
 
 
 // Setup Wifi and NTP Server +++++++++++++++++
@@ -14,7 +15,7 @@ unsigned int localPort = 2390;      // local port to listen for UDP packets
 
 const char* ntpServerName = "0.at.pool.ntp.org";
 
-const int UTCTimeoffset = 1;    // Timezone offset compared to UTC at Greenwich Meridian (GMT)
+const int UTCTimeoffset = 2;    // Timezone offset compared to UTC at Greenwich Meridian (GMT)
 
 
 // Pindefinitions Stepperdriver
@@ -23,27 +24,28 @@ const int UTCTimeoffset = 1;    // Timezone offset compared to UTC at Greenwich 
 #define Stepper_en 14   // enable
 
 // constants 
-const int CW = 1, CCW = -1;
-const int Clock_direction = CW;   // Set direction: CW is clockwise, CCW ist anti clockwise 
+const byte CW = -1, CCW = 1;
+const byte Clock_direction = CW;   // Set direction: CW is clockwise, CCW ist anti clockwise 
 
 
 // Setup Stepperdriver ++++++++++++
-#define Stepper_Speed_Initial 17          // initial motor speed (when setting time)
-#define Stepper_Speed 0.25          // motor speed
+#define Stepper_Speed_Initial 8          // initial motor speed (when setting time)
+#define Stepper_Speed 2         // motor speed
 #define MICROSTEPS 16    // microsteps of the stepper driver
-#define MOTOR_STEPS (2038 / MICROSTEPS) // steps and gearing of steper motor
+#define MOTOR_STEPS 2038 // steps and gearing of steper motor
 
 // initialize stepper class (no, of steps per revolution, direction and step pin
-A4988 stepper(MOTOR_STEPS, Stepper_dir, Stepper_step);
+BasicStepperDriver stepper(MOTOR_STEPS, Stepper_dir, Stepper_step);
 
 // Set the number of steps to move minute handle by one minute
-const unsigned long stepper_oneMin = 512; // measured in practice
-const unsigned long stepper_halfMin = 5124.0; //10 min 
+const unsigned long stepper_oneMin = 4096.0; // measured in practice
+const unsigned long stepper_halfMin = 40965.0; //10 min 
 const unsigned long stepper_quarterMin = 2048; //4 min
 
-// unsigned long moving_threshold = 15000;  // move minute handle every 15 seconds (15000 ms)
+unsigned long moving_threshold = 60;  // move minute handle every 60 seconds 
 
-// unsigned long previousTime = 0;
+unsigned long previousTime = 0;
+// unsigned long currentTime = 0;
 
 
 void setup() {
@@ -53,7 +55,7 @@ void setup() {
   // Start stepper driver 
   stepper.begin(Stepper_Speed_Initial, MICROSTEPS);
   pinMode(Stepper_en, OUTPUT);
-  digitalWrite(Stepper_en, LOW);
+  digitalWrite(Stepper_en, HIGH);
 
   // Connect to WiFi
   WifiConnect(ssid, pass);
@@ -61,20 +63,23 @@ void setup() {
   // Get current Time from NTP Server and correct time
   int time = getTime(localPort, UTCTimeoffset, ntpServerName);
   Serial.println(time);
-  int now = TimeCorrection(stepper_halfMin/600.0, time, 3.96*Stepper_Speed_Initial);
+  int now = TimeCorrection(stepper_halfMin/600.0, time, 62.65);
   Serial.println(now);
 
-  // Disconnect Wifi and enter modem sleep
+  // Disconnect Wifi and enter modem sleep, drops 75 mA
   WifiDisconnect();
 
   // move to desired time  
-  if (time <= 21276){
-    stepper.move(Clock_direction*now);
-  }
-  else{
-    stepper.move(-Clock_direction*now);
-  }
+  // digitalWrite(Stepper_en, LOW);
+  // if (time <= 21276){
+  //   stepper.move(Clock_direction*now);
+  // }
+  // else{
+  //   stepper.move(-Clock_direction*now);
+  // }
   stepper.setRPM(Stepper_Speed);
+  // digitalWrite(Stepper_en, HIGH);
+  // previousTime = millis();
 }
 
 
@@ -87,19 +92,44 @@ void incrementHalfMinute() {
 }
 
 void incrementOneMinute() {
-  stepper.move(Clock_direction*stepper_oneMin);
+  digitalWrite(Stepper_en, LOW);
+  stepper.move(Clock_direction*stepper_oneMin/60);
+  digitalWrite(Stepper_en, HIGH);
+}
+
+void fpm_wakup_cb_func(void) {
+  Serial.println("Light sleep is over, either because timeout or external interrupt");
+  Serial.flush();
+}
+
+void light_sleep(unsigned long timeout){
+  extern os_timer_t *timer_list;
+  timer_list = nullptr;
+
+  wifi_fpm_set_sleep_type(LIGHT_SLEEP_T);
+  wifi_fpm_open();
+  wifi_fpm_set_wakeup_cb(fpm_wakup_cb_func);
+  // light sleep function requires microseconds
+  wifi_fpm_do_sleep(timeout * 1e6);
+  delay(timeout + 1);
 }
 
 
 void loop() {
   // unsigned long currentTime = millis();
-  stepper.move(Clock_direction*stepper_halfMin);
-
-  // check if 15 seconds passed
-  // if ((unsigned long)(currentTime - previousTime) >= moving_threshold) {
-  //   previousTime = currentTime;
-  //   incrementQuarterMinute(); // this takes some time
+  // Serial.print(currentTime);
+  // Serial.print("\t");
+  
+    // previousTime = currentTime;
+    // incrementQuarterMinute(); // this takes some time
     // incrementHalfMinute();
-    // incrementOneMinute();
-  // }
+    incrementOneMinute();
+    // Serial.print(millis() - currentTime);
+    // Serial.print("\t");
+    // ESP.deepSleep(10e6);
+    light_sleep(10);    
+    // Serial.print(currentTime - millis());
+    // Serial.print("\t");
+    // yield();
+    // Serial.println(currentTime - millis());
 }
