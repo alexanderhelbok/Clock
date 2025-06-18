fb7686f55510f0d27038e1938b8a225f63c5d617
Alexander Helbok <alexander.helbok@hotmail.com>
2025-06-18 16:43:28 +0200

    implemented Hall sensor feedback

--- include/Calc.h
+++ include/Calc.h
@@ -1,4 +1,4 @@
 #include <iostream>
 #include <math.h>
 
-int TimeCorrection(double offset, double time, double speed);
+int TimeCorrection(double time, double speed);
--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,10 @@
 
 
 // Setup Wifi and NTP Server +++++++++++++++++
-char ssid[] = "Wirtschaftshilfe fuer Studenten";  //  your network SSID (name)
-char pass[] = "9ter12ter1949";       // your network password
+// char ssid[] = "Wirtschaftshilfe fuer Studenten";  //  your network SSID (name)
+// char pass[] = "9ter12ter1949";       // your network password
+char ssid[] = "Free VBucks";  //  your network SSID (name)
+char pass[] = "Hallo123";       // your network password
 
 unsigned int localPort = 2390;      // local port to listen for UDP packets
 
@@ -22,6 +24,9 @@ const int UTCTimeoffset = 2;    // Timezone offset compared to UTC at Greenwich 
 #define Stepper_dir 13  // direction
 #define Stepper_step 12 // step
 #define Stepper_en 14   // enable
+
+#define Hall_Hour_pin 5 // Hall sensor pin
+#define Hall_Minute_pin 4 // Hall sensor pin
 
 // constants 
 const int CW = -1, CCW = 1;
@@ -38,17 +43,19 @@ const int Clock_direction = CW;   // Set direction: CW is clockwise, CCW ist ant
 BasicStepperDriver stepper(MOTOR_STEPS, Stepper_dir, Stepper_step);
 
 // Set the number of steps to move minute handle by one minute
-const unsigned long stepper_oneMin = 4096.0; // measured in practice
-const unsigned long stepper_halfMin = 40965.0; //10 min 
-const unsigned long stepper_quarterMin = 2048; //4 min
+const unsigned long stepper_oneMin = 4096; // measured in practice
+const unsigned long stepper_halfMin = 2048; // half min
+const unsigned long stepper_quarterMin = 1024; //4 min
 
-unsigned long moving_threshold = 60;  // move minute handle every 60 seconds 
+const unsigned long stepper_oneSec = stepper_oneMin/60; // steps per second for one minute 
 
-unsigned long previousTime = 0;
-unsigned long currentTime = 0;
+unsigned int currentHour = 0;
+unsigned int currentMinute = 0;
 
-// void incrementHalfMinute();
-// void incrementOneMinute();
+unsigned long sleep_time = 66170;
+
+void incrementOneMinute();
+void incrementHalfMinute();
 
 void setup() {
   Serial.begin(115200);
@@ -57,16 +64,34 @@ void setup() {
   // Start stepper driver 
   stepper.begin(Stepper_Speed_Initial, MICROSTEPS);
   pinMode(Stepper_en, OUTPUT);
-  digitalWrite(Stepper_en, HIGH);
+  digitalWrite(Stepper_en, LOW);
+
+  // turn the stepper until both are at 12
+  while ((digitalRead(Hall_Minute_pin) || digitalRead(Hall_Hour_pin))) {
+    stepper.move(Clock_direction*stepper_quarterMin);
+  }
+  // compensate for Hall sensor offset
+  incrementHalfMinute(); 
 
   // Connect to WiFi
   WifiConnect(ssid, pass);
 
   // Get current Time from NTP Server and correct time
   int time = getTime(localPort, UTCTimeoffset, ntpServerName);
-  Serial.println(time);
-  int now = TimeCorrection(stepper_halfMin/600.0, time, 62.65);
-  Serial.println(now);
+  Serial.println("Current time from NTP server: ");
+  // print time in HH:MM format
+  Serial.print(time / 3600);
+  Serial.print(":");
+  Serial.println((time / 60) % 60);
+
+  int now = TimeCorrection(time, 62.65);
+
+  Serial.println("Corrected time: ");
+  // print corrected time in HH:MM format
+  Serial.print(now / 3600);
+  Serial.print(":");
+  Serial.println((now / 60) % 60);
+
 
   // Disconnect Wifi and enter modem sleep, drops 75 mA
   WifiDisconnect();
@@ -74,19 +99,24 @@ void setup() {
   // move to desired time  
   digitalWrite(Stepper_en, LOW);
   if (time <= 21276){
-    stepper.move(Clock_direction*now);
+    stepper.move(Clock_direction*now*stepper_oneSec);
   }
   else{
-    stepper.move(-Clock_direction*now);
+    stepper.move(-Clock_direction*now*stepper_oneSec);
   }
   stepper.setRPM(Stepper_Speed);
   digitalWrite(Stepper_en, HIGH);
-  // previousTime = millis();
+  currentHour = (now / 3600) % 24; // calculate current hour
+  currentMinute = (now / 60) % 60; // calculate current minute
+
+  light_sleep(sleep_time);
 }
 
 
 void incrementQuarterMinute() {
+  digitalWrite(Stepper_en, LOW);
   stepper.move(Clock_direction*stepper_quarterMin);
+  digitalWrite(Stepper_en, HIGH);
 }
 
 void incrementHalfMinute() {
@@ -99,18 +129,59 @@ void incrementOneMinute() {
   digitalWrite(Stepper_en, LOW);
   stepper.move(Clock_direction*stepper_oneMin);
   digitalWrite(Stepper_en, HIGH);
+
+  currentMinute += 1;
+  if (currentMinute >= 60) {
+    currentMinute = 0;
+    currentHour += 1;
+    if (currentHour >= 24) {
+      currentHour = 0;
+    }
+  }
+}
+
+void stepperMoveFast(unsigned long steps) {
+  digitalWrite(Stepper_en, LOW);
+  stepper.setRPM(Stepper_Speed_Initial);
+  stepper.move(Clock_direction * steps);
+  stepper.setRPM(Stepper_Speed);
+  digitalWrite(Stepper_en, HIGH);
 }
 
 void loop() {
-  // currentTime = RTCmillis();
-  // Serial.println(RTCmillis() - previousTime);
-  // previousTime = currentTime;
   incrementOneMinute();
-  // Serial.print(RTCmillis() - currentTime);
-  // Serial.print("\t");
-  // currentTime = RTCmillis();
-  light_sleep(66170);
-  // Serial.print(RTCmillis() - currentTime);
-  // Serial.print("\t");
+  // Serial.print("Current time: ");
+  // Serial.print(currentHour);
+  // Serial.print(":");
+  // Serial.println(currentMinute);
+
+  // Serial.print("Hall Hour: ");
+  // Serial.println(digitalRead(Hall_Hour_pin));
+  // Serial.print("Hall Minute: ");
+  // Serial.println(digitalRead(Hall_Minute_pin));
+  
+  if (!digitalRead(Hall_Hour_pin) && !digitalRead(Hall_Minute_pin)) {
+    // compare time with NTP server
+    WifiConnect(ssid, pass);
+    int time = getTime(localPort, UTCTimeoffset, ntpServerName);
+    WifiDisconnect();
+    // Serial.print("Current time: ");
+    // Serial.println(time);
+    int hour = (time / 3600) % 24; // calculate current hour
+    int minute = (time / 60) % 60; // calculate current minute
+
+    // update current time
+    currentHour = hour;
+    currentMinute = minute;
+
+    int timeDiff = (hour % 12) * 60 + minute;
+
+    // Serial.print("Time difference: ");
+    // Serial.println(timeDiff);
+
+    stepperMoveFast(stepper_oneMin * timeDiff);
+  }
+
+  light_sleep(sleep_time);
   // ESP.deepSleep(10e6);
 }
